0facf667a1583a9b78f8ac3294f4416d7a4842f8
JonathanESchmidt <jschmi17@student.aau.dk>
2023-05-05 15:44:40 +0200

    Created class for calling detect when needed

--- detectContext.py
+++ detectContext.py
@@ -13,83 +13,86 @@ from utils.general import check_img_size, non_max_suppression, apply_classifier,
 from utils.torch_utils import select_device, load_classifier, time_synchronized, TracedModel
 
 
-def detect():
-    weights, imgsz, trace =  opt.weights, opt.img_size, not opt.no_trace
+class Detector():
+    def __init__(self):
+        self.weights, self.imgsz, self.trace =  opt.weights, opt.img_size, not opt.no_trace
+        self.augment, self.conf_thres, self.iou_thres = opt.augment, opt.conf_thres, opt.iou_thres
+        self.classes, self.agnostic_nms = opt.classes, opt.agnostic_nms
+        # Initialize
+        set_logging()
+        self.device = select_device(opt.device)
+        self.half = self.device.type != 'cpu'  # half precision only supported on CUDA
 
-    # Initialize
-    set_logging()
-    device = select_device(opt.device)
-    half = device.type != 'cpu'  # half precision only supported on CUDA
+        # Load model
+        self.model = attempt_load(self.weights, map_location=self.device)  # load FP32 model
+        self.stride = int(self.model.stride.max())  # model stride
+        self.imgsz = check_img_size(self.imgsz, s=self.stride)  # check img_size
 
-    # Load model
-    model = attempt_load(weights, map_location=device)  # load FP32 model
-    stride = int(model.stride.max())  # model stride
-    imgsz = check_img_size(imgsz, s=stride)  # check img_size
+        if self.trace:
+            self.model = TracedModel(self.model, self.device, self.imgsz)
 
-    if trace:
-        model = TracedModel(model, device, opt.img_size)
+        if self.half:
+            self.model.half()  # to FP16
 
-    if half:
-        model.half()  # to FP16
+        # Second-stage classifier
+        self.classify = False
+        if self.classify:
+            self.modelc = load_classifier(name='resnet101', n=2)  # initialize
+            self.modelc.load_state_dict(torch.load('weights/resnet101.pt', map_location=self.device)['model']).to(self.device).eval()
 
-    # Second-stage classifier
-    classify = False
-    if classify:
-        modelc = load_classifier(name='resnet101', n=2)  # initialize
-        modelc.load_state_dict(torch.load('weights/resnet101.pt', map_location=device)['model']).to(device).eval()
+        # Get names and colors
+        self.names = self.model.module.names if hasattr(self.model, 'module') else self.model.names
 
-    # Get names and colors
-    names = model.module.names if hasattr(model, 'module') else model.names
+        # Run inference
+        if self.device.type != 'cpu':
+            self.model(torch.zeros(1, 3, self.imgsz, self.imgsz).to(self.device).type_as(next(self.model.parameters())))  # run once
+        self.old_img_w = self.old_img_h = self.imgsz
+        self.old_img_b = 1
 
-    # Run inference
-    if device.type != 'cpu':
-        model(torch.zeros(1, 3, imgsz, imgsz).to(device).type_as(next(model.parameters())))  # run once
-    old_img_w = old_img_h = imgsz
-    old_img_b = 1
 
-    new = input("Put image path:\n") # Remove after ROS implementation
-    while new: # Remove after ROS implementation - change to callback func
-        
-        path = Path(new) # Remove after ROS implementation
+
+    def detect(self, img_path):
+   
+        path = Path(img_path) # Remove after ROS implementation
 
         for image in path.iterdir(): # Remove after ROS implementation
             # TODO change to read images from ROS2
             im0s = cv2.imread(str(image))  # BGR # Remove after ROS implementation
             assert im0s is not None, 'Image Not Found ' + path
 
-            img = letterbox(im0s, imgsz, stride=stride)[0]
+            img = letterbox(im0s, self.imgsz, stride=self.stride)[0]
 
             # Convert
             img = img[:, :, ::-1].transpose(2, 0, 1)  # BGR to RGB, to 3x416x416
             img = np.ascontiguousarray(img)
 
-            img = torch.from_numpy(img).to(device)
-            img = img.half() if half else img.float()  # uint8 to fp16/32
+            img = torch.from_numpy(img).to(self.device)
+            img = img.half() if self.half else img.float()  # uint8 to fp16/32
             img /= 255.0  # 0 - 255 to 0.0 - 1.0
             if img.ndimension() == 3:
                 img = img.unsqueeze(0)
 
             # Warmup
-            if device.type != 'cpu' and (old_img_b != img.shape[0] or old_img_h != img.shape[2] or old_img_w != img.shape[3]):
-                old_img_b = img.shape[0]
-                old_img_h = img.shape[2]
-                old_img_w = img.shape[3]
+            if self.device.type != 'cpu' and (self.old_img_b != img.shape[0] or self.old_img_h != img.shape[2] or self.old_img_w != img.shape[3]):
+                self.old_img_b = img.shape[0]
+                self.old_img_h = img.shape[2]
+                self.old_img_w = img.shape[3]
                 for i in range(3):
-                    model(img, augment=opt.augment)[0]
+                    self.model(img, augment=self.augment)[0]
 
             # Inference
             t1 = time_synchronized()
             with torch.no_grad():   # Calculating gradients would cause a GPU memory leak
-                pred = model(img, augment=opt.augment)[0]
+                pred = self.model(img, augment=self.augment)[0]
             t2 = time_synchronized()
 
             # Apply NMS
-            pred = non_max_suppression(pred, opt.conf_thres, opt.iou_thres, classes=opt.classes, agnostic=opt.agnostic_nms)
+            pred = non_max_suppression(pred, self.conf_thres, self.iou_thres, classes=self.classes, agnostic=self.agnostic_nms)
             t3 = time_synchronized()
 
             # Apply Classifier
-            if classify:
-                pred = apply_classifier(pred, modelc, img, im0s)
+            if self.classify:
+                pred = apply_classifier(pred, self.modelc, img, im0s)
 
             # Process detections
             for i, det in enumerate(pred):  # detections per image
@@ -104,12 +107,12 @@ def detect():
                     # Print results
                     for c in det[:, -1].unique():
                         n = (det[:, -1] == c).sum()  # detections per class
-                        s += f"{n} {names[int(c)]}{'s' * (n > 1)}, "  # add to string
+                        s += f"{n} {self.names[int(c)]}{'s' * (n > 1)}, "  # add to string
 
                     # Write results
                     for *xyxy, conf, cls in reversed(det):
                         xywh = (xyxy2xywh(torch.tensor(xyxy).view(1, 4)) / gn).view(-1).tolist()  # normalized xywh
-                        line = (cls, *xywh, conf) # if opt.save_conf else (cls, *xywh)  # label format
+                        line = (cls, *xywh, conf)
                         output = (('%g ' * len(line)).rstrip() % line).split(' ')
                         
                         ID = output[0]
@@ -119,12 +122,12 @@ def detect():
                         h = output[4]
                         confi = output[5]
 
-                        print(f"All = {ID}, {x}, {y}, {w}, {h}, {confi}") # Remove after ROS implementation
+                        print(f"Class = {ID}, x = {x}, y = {y}, width = {w}, height = {h}, conf = {confi}") # Remove after ROS implementation
 
                 # Print time (inference + NMS)
                 print(f'{s}Done. ({(1E3 * (t2 - t1)):.1f}ms) Inference, ({(1E3 * (t3 - t2)):.1f}ms) NMS')
 
-        new = input("Put image path:\n") # Remove after ROS implementation
+
 
 
 if __name__ == '__main__':
@@ -134,24 +137,18 @@ if __name__ == '__main__':
     parser.add_argument('--conf-thres', type=float, default=0.25, help='object confidence threshold')
     parser.add_argument('--iou-thres', type=float, default=0.45, help='IOU threshold for NMS')
     parser.add_argument('--device', default='', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
-    parser.add_argument('--save-conf', action='store_true', help='save confidences in --save-txt labels')
-    parser.add_argument('--nosave', action='store_true', help='do not save images/videos')
     parser.add_argument('--classes', nargs='+', type=int, help='filter by class: --class 0, or --class 0 2 3')
     parser.add_argument('--agnostic-nms', action='store_true', help='class-agnostic NMS')
     parser.add_argument('--augment', action='store_true', help='augmented inference')
-    parser.add_argument('--update', action='store_true', help='update all models')
-    parser.add_argument('--project', default='runs/detect', help='save results to project/name')
-    parser.add_argument('--name', default='exp', help='save results to project/name')
-    parser.add_argument('--exist-ok', action='store_true', help='existing project/name ok, do not increment')
     parser.add_argument('--no-trace', action='store_true', help='don`t trace model')
     opt = parser.parse_args()
-    print(opt)
+    #print(opt)
     #check_requirements(exclude=('pycocotools', 'thop'))
 
     with torch.no_grad():
-        if opt.update:  # update all models (to fix SourceChangeWarning)
-            for opt.weights in ['yolov7.pt']:
-                detect()
-                strip_optimizer(opt.weights)
-        else:
-            detect()
+        
+        detector = Detector()
+        img_path = input("Put image path:\n") # Remove after ROS implementation
+        while img_path: # Remove after ROS implementation - change to callback func
+            detector.detect(img_path)
+            img_path = input("Put image path:\n") # Remove after ROS implementation
