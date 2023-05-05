09784734f927f1c5fbf9e674eb6fa4b6b9d7bd01
JonathanESchmidt <jschmi17@student.aau.dk>
2023-05-05 13:11:51 +0200

    Cleanup imports

--- detectContext.py
+++ detectContext.py
@@ -9,10 +9,10 @@ import numpy as np
 from numpy import random
 
 from models.experimental import attempt_load
-from utils.datasets import LoadStreams, LoadImages, letterbox
-from utils.general import check_img_size, check_requirements, check_imshow, non_max_suppression, apply_classifier, \
+from utils.datasets import letterbox
+from utils.general import check_img_size, non_max_suppression, apply_classifier, \
     scale_coords, xyxy2xywh, strip_optimizer, set_logging, increment_path
-from utils.plots import plot_one_box
+
 from utils.torch_utils import select_device, load_classifier, time_synchronized, TracedModel
 
 
